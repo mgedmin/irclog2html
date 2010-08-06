2c51f967c75de1db997a5ed92ac8369174059107
Marius Gedminas <marius@gedmin.as>
2010-08-06 04:33:46 +0300

    Post-release version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = read('README.txt') + '\n\n' + read('CHANGES.txt')
 
 setup(
     name='irclog2html',
-    version='2.9.1',
+    version='2.9.2dev',
     author='Marius Gedminas',
     author_email='marius@gedmin.as',
     license='GPL',
