3a578eff7202156e767107743489c3cc49cd36ac
Marius Gedminas <marius@gedmin.as>
2015-09-17 16:18:49 +0300

    Add the forgotten irclogserver module (augh)

---
+++ src/irclog2html/irclogserver.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+"""
+Serve IRC logs (WSGI app)
+
+Expects to find logs matching the IRCLOG_GLOB pattern (default: *.log)
+in the directory specified by the IRCLOG_LOCATION environment variable.
+Expects the filenames to contain a ISO 8601 date (YYYY-MM-DD).
+
+Apache configuration example:
+
+  WSGIScriptAlias /irclogs /path/to/irclogserver.py
+  <Location /irclogs>
+    SetEnv IRCLOG_LOCATION /path/to/irclog/files/
+    # Uncomment the following if your log files use a different format
+    #SetEnv IRCLOG_GLOB "*.log.????-??-??"
+  </Location>
+
+"""
+
+# Copyright (c) 2015, Marius Gedminas and contributors
+#
+# Released under the terms of the GNU GPL
+# http://www.gnu.org/copyleft/gpl.html
+
+import cgi
+import io
+import os
+
+from .irclog2html import CSS_FILE
+from .irclogsearch import (
+    DEFAULT_LOGFILE_PATH, DEFAULT_LOGFILE_PATTERN, search_page,
+)
+
+
+def get_path(environ):
+    path = environ.get('PATH_INFO', '/')
+    path = path[1:]  # Remove the leading slash
+    if '/' in path or '\\' in path:
+        return None
+    return path if path != '' else 'index.html'
+
+
+def wsgi(environ, start_response):
+    """WSGI application"""
+    logfile_path = environ.get('IRCLOG_LOCATION') or DEFAULT_LOGFILE_PATH
+    logfile_pattern = environ.get('IRCLOG_GLOB') or DEFAULT_LOGFILE_PATTERN
+    form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+    stream = io.TextIOWrapper(io.BytesIO(), 'ascii',
+                              errors='xmlcharrefreplace',
+                              line_buffering=True)
+
+    status = "200 Ok"
+    content_type = "text/html; charset=UTF-8"
+    headers = {}
+
+    path = get_path(environ)
+    if path is None:
+        status = "404 Not Found"
+        result = [b"Not found"]
+    elif path == 'search':
+        fmt = search_page(stream, form, logfile_path, logfile_pattern)
+        result = [stream.buffer.getvalue()]
+        del fmt
+    elif path == 'irclog.css':
+        content_type = "text/css"
+        try:
+            with open(CSS_FILE, "rb") as f:
+                result = [f.read()]
+        except IOError:
+            status = "404 Not Found"
+            result = [b"Not found"]
+    else:
+        if path.endswith('.css'):
+            content_type = "text/css"
+        if path.endswith('.log') or path.endswith('.txt'):
+            content_type = "text/plain"
+        try:
+            with open(os.path.join(logfile_path, path), "rb") as f:
+                result = [f.read()]
+        except IOError:
+            if path == 'index.html':
+                # no index? redirect to search page
+                status = "302 Found"
+                result = [b"Try /search"]
+                headers['Location'] = '/search'
+            else:
+                status = "404 Not Found"
+                result = [b"Not found"]
+
+    headers["Content-Type"] = content_type
+    # We need str() for Python 2 because of unicode_literals
+    headers = sorted((str(k), str(v)) for k, v in headers.items())
+    start_response(str(status), headers)
+    return result
+
+
+def main():  # pragma: nocover
+    """Simple web server for manual testing"""
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, wsgi)
+    print("Started at http://localhost:8080/")
+    srv.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/irclog2html/tests/test_irclogserver.py
@@ -0,0 +1,178 @@
+import doctest
+import gzip
+import os
+import re
+import shutil
+import sys
+import tempfile
+from contextlib import closing
+
+import mock
+from zope.testing import renormalizing
+
+from irclog2html.irclogserver import get_path, wsgi
+
+
+here = os.path.dirname(__file__)
+
+
+def gzip_copy(src, dst):
+    with open(src, 'rb') as fi:
+        with closing(gzip.open(dst, 'wb')) as fo:
+            shutil.copyfileobj(fi, fo)
+
+
+def set_up_sample():
+    tmpdir = tempfile.mkdtemp(prefix='irclog2html-test-')
+    gzip_copy(os.path.join(here, 'sample.log'),
+              os.path.join(tmpdir, 'sample-2013-03-17.log.gz'))
+    shutil.copy(os.path.join(here, 'sample.log'),
+                os.path.join(tmpdir, 'sample-2013-03-18.log'))
+    with open(os.path.join(tmpdir, "index.html"), "w") as f:
+        f.write("This is the index")
+    with open(os.path.join(tmpdir, "font.css"), "w") as f:
+        f.write("* { font: comic sans; }")
+    return tmpdir
+
+
+def clean_up_sample(tmpdir):
+    shutil.rmtree(tmpdir)
+
+
+def doctest_get_path():
+    """Test for get_path.
+
+    This function decides whether to search or to display a file based
+    on URL path:
+
+        >>> get_path(dict(PATH_INFO='/search'))
+        'search'
+
+        >>> get_path(dict(PATH_INFO='/#channel-2015-05-05.log.html'))
+        '#channel-2015-05-05.log.html'
+
+    When there is no file name, we show the index:
+
+        >>> get_path(dict(PATH_INFO='/'))
+        'index.html'
+
+    Any slashes other than the leading one result in None:
+
+        >>> get_path(dict(PATH_INFO='/../../etc/passwd'))
+
+    """
+
+
+def doctest_wsgi():
+    r"""Test for the wsgi entry point
+
+        >>> tmpdir = set_up_sample()
+        >>> start_response = mock.MagicMock()
+
+        >>> environ = {
+        ...     'IRCLOG_LOCATION': tmpdir,
+        ...     'PATH_INFO': "/",
+        ...     'wsgi.input': None,
+        ... }
+
+    When accessing the root, we get the index:
+
+        >>> wsgi(environ, start_response)
+        [b'This is the index']
+        >>> start_response.assert_called_once_with(
+        ...     '200 Ok', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    We can load the stylesheet too, even if it's not copied to the log
+    directory:
+
+        >>> environ['PATH_INFO'] = '/irclog.css'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'...div.searchbox {...']
+        >>> start_response.assert_called_once_with(
+        ...     '200 Ok', [('Content-Type', 'text/css')])
+
+    We can load other CSS files, if needed
+
+        >>> environ['PATH_INFO'] = '/font.css'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'* { font: comic sans; }']
+        >>> start_response.assert_called_once_with(
+        ...     '200 Ok', [('Content-Type', 'text/css')])
+
+    Accessing the search page:
+
+        >>> environ['PATH_INFO'] = '/search'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'<!DOCTYPE html PUBLIC...<title>Search IRC logs</title>...
+        >>> start_response.assert_called_once_with(
+        ...    '200 Ok', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    Searching the logs:
+
+        >>> environ['PATH_INFO'] = '/search'
+        >>> environ['QUERY_STRING'] = 'q=bot'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'...<p>10 matches in 2 log files with 20 lines (... seconds).</p>...
+        >>> start_response.assert_called_once_with(
+        ...    '200 Ok', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    Retrieving log files:
+
+        >>> environ['PATH_INFO'] = '/sample-2013-03-18.log'
+        >>> del environ['QUERY_STRING']
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'2005-01-08T23:33:54 *** povbot has joined #pov...
+        >>> start_response.assert_called_once_with(
+        ...    '200 Ok', [('Content-Type', 'text/plain')])
+
+    Accessing paths with slashes:
+
+        >>> environ['PATH_INFO'] = '/./index.html'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'Not found']
+        >>> start_response.assert_called_once_with(
+        ...    '404 Not Found', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    What if some poor soul runs Windows?
+
+        >>> environ['PATH_INFO'] = '/.\\index.html'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'Not found']
+        >>> start_response.assert_called_once_with(
+        ...    '404 Not Found', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    Accessing non-existing files:
+
+        >>> environ['PATH_INFO'] = '/nonexistent'
+        >>> start_response = mock.MagicMock()
+        >>> wsgi(environ, start_response)
+        [b'Not found']
+        >>> start_response.assert_called_once_with(
+        ...    '404 Not Found', [('Content-Type', 'text/html; charset=UTF-8')])
+
+    Clean up:
+
+        >>> clean_up_sample(tmpdir)
+
+    """
+
+
+checker = None
+if sys.version_info[0] == 2:
+    checker = renormalizing.RENormalizing([
+        (re.compile(r"^\['"), r"[b'"),
+        (re.compile(r"u('.*?')"), r"\1"),
+    ])
+
+
+def test_suite():
+    optionflags = (doctest.ELLIPSIS | doctest.REPORT_NDIFF |
+                   doctest.NORMALIZE_WHITESPACE)
+    return doctest.DocTestSuite(optionflags=optionflags, checker=checker)
