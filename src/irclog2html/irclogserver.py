727afd17e264737f3c9eb9b5de4c58ca39b925a9
Marius Gedminas <marius@gedmin.as>
2015-09-18 09:37:40 +0300

    Experiment: render HTML logs dynamically
    Missing pieces:
    - error handling when the log file doesn't exist
    - next/prev/index navigation
    - rendering the index page
    - making it possible to customize the style/colors/title format/etc

--- src/irclog2html/irclogserver.py
+++ src/irclog2html/irclogserver.py
@@ -33,7 +33,9 @@ try:
 except ImportError:
     from urllib.parse import quote_plus # Py3
 
-from .irclog2html import CSS_FILE, LogParser
+from .irclog2html import (
+    CSS_FILE, LogParser, XHTMLTableStyle, convert_irc_log,
+)
 from .irclogsearch import (
     DEFAULT_LOGFILE_PATH, DEFAULT_LOGFILE_PATTERN, search_page,
     HEADER, FOOTER
@@ -123,6 +125,15 @@ def application(environ, start_response):
                 result = [b"Try /search"]
                 headers['Location'] = '/search'
                 content_type = "text/plain"
+            elif path.endswith('.html'):
+                buf = io.BytesIO()
+                with open(os.path.join(logfile_path, path[:-5]), 'rb') as f:
+                    parser = LogParser(f)
+                    formatter = XHTMLTableStyle(buf)
+                    convert_irc_log(parser, formatter, path[:-5],
+                                    ('', ''), ('', ''), ('', ''),
+                                    searchbox=True)
+                    result = [buf.getvalue()]
             else:
                 status = "404 Not Found"
                 result = [b"Not found"]
